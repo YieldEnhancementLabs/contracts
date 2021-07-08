b81427ed76a3fe2067175b667fb3e62d27714a80
kobs30 <sergeik30@gmail.com>
2021-07-08 20:08:36 +0200

    initial commit

---
+++ migrations/3_deploy_GeneticAndGenome.js
@@ -0,0 +1,11 @@
+//const GeneticAndGenome = artifacts.require("GeneticAndGenome");
+
+
+// deployed contract 0x7815bDa662050D84718B988735218CFfd32f75ea
+//          develop 0x66d46bF771C3D2b44248554F27816CFECda5795E
+// token
+//          develop 0x10DbF313ebcbf264c87CA6eb445CaA860C0856B8
+//          product 0xD3b71117E6C1558c1553305b44988cd944e97300
+module.exports = function(deployer) {
+   //deployer.deploy(GeneticAndGenome, "0x10DbF313ebcbf264c87CA6eb445CaA860C0856B8", 0, 1625047200); // https://www.unixtimestamp.com/
+};
---
+++ migrations/5_deploy_Plasmide.js
@@ -0,0 +1,9 @@
+//const Plasmide = artifacts.require("Plasmide");
+
+module.exports = function(deployer) {
+//   deployer.deploy(Plasmide,
+//       "0x10DbF313ebcbf264c87CA6eb445CaA860C0856B8", // "0x7815bDa662050D84718B988735218CFfd32f75ea", // YEL token
+//       0, // yelPerSecond
+//       1625050800 // Start Time
+//     );
+};
